468bbb9d5f4d5078af23444663b70d41837d22b3
jcori <gonzalocori@gmail.com>
2024-11-18 21:37:12 -0400

    recuperando data of back

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:app_map/pages/page2.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:url_launcher/url_launcher.dart';
+
+import 'package:app_map/services/location_service.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -16,15 +21,26 @@ class _HomeState extends State<Home> {
 
   MapController _mapController = MapController();
 
-  var lat = -16.495991;
-  var lng = -68.134120;
+  var lat = -17.013472;
+  var lng = -64.192689;
+  //List<Map<String, dynamic>> locations = [];
+  List locations = [];
+  List<LatLng> polylinePoints = [];
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();    
     permission();
-    miubicacion();
+    //miubicacion();
+    getLocations();
+  }
+
+  getLocations() async {
+    var locations = await LocationService().getLocations();
+    print(locations);
+    setState(() {
+      this.locations = locations;
+    });
   }
 
   permission() async{
@@ -40,7 +56,7 @@ class _HomeState extends State<Home> {
       lat = position.latitude;
       lng = position.longitude;
     });
-    _mapController.move(LatLng(lat,lng), 13);
+    _mapController.move(LatLng(lat,lng), 17);
   }
 
   @override
@@ -48,40 +64,130 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Hello Home 1234'),        
+        title: const Text('Home'),        
       ),
-      body: FlutterMap(
-        mapController: _mapController,
-        options: MapOptions(
-          initialCenter: LatLng(lat, lng), // Center the map over London
-          initialZoom: 13,
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text('Mi ubicación'),
+              onTap: () {
+                miubicacion();
+              },
+            ),
+            ListTile(
+              title: const Text('Pagina 2'),
+              onTap: () {
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Page2())
+                );
+              },
+            ),
+          ],
         ),
+      ),
+      body: Column(
         children: [
-          TileLayer( // Display map tiles from any source
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
-            userAgentPackageName: 'com.example.app',
-            // And many more recommended properties!
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: locations.length,
+            itemBuilder: (context, index){ 
+              return ElevatedButton(
+                onPressed: () async {
+
+                  var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+                  print('position >>> ${position}');
+                  //String url = 'https://router.project-osrm.org/route/v1/driving/${position.longitude},${position.latitude};${locations[index]['longitude']},${locations[index]['latitude']}?geometries=geojson';
+                  
+                  print('longitud origen >>> ${lng}');
+                  print('latitud origen >>> ${lat}');
+                  print('longitud destino >>> ${locations[index]['longitude']}');
+                  print('latitud destino >>> ${locations[index]['latitude']}');
+
+                  if (position.longitude != null && position.latitude != null && locations[index]['longitude'] != null && locations[index]['latitude'] != null) {
+                    //String url = 'https://router.project-osrm.org/route/v1/driving/${position.longitude},${position.latitude};${locations[index]['longitude']},${locations[index]['latitude']}?geometries=geojson';
+                    String url = 'https://router.project-osrm.org/route/v1/driving/${lng},${lat};${locations[index]['longitude'].toString()},${locations[index]['latitude'].toString()}?geometries=geojson';
+                    final response = await http.get(Uri.parse(url));
+                    print('Estado response >>> ${response.statusCode}');
+                    if(response.statusCode == 200){
+                      final data = json.decode(response.body);
+                      final List<dynamic> coordinates = data['routes'][0]['geometry']['coordinates'];
+
+                      setState(() {
+                      polylinePoints = coordinates
+                        .map((point) => LatLng(point[1], point[0]))
+                        .toList();
+                      });
+
+                    } else {
+                      print('Error al obtener la ruta: ${response.statusCode}');
+                    }
+
+                  } else {
+                    print('Error: Alguna coordenada es nula');
+                  }
+
+                  setState(() {
+                     lat = double.parse(locations[index]['latitude']);
+                     lng = double.parse(locations[index]['longitude']);
+                   });
+                   _mapController.move(LatLng(lat, lng), 6);
+                }, 
+                child: Text(locations[index]['name']),
+              );
+            },
           ),
 
-          MarkerLayer(
-            markers:  [
-              Marker(
-                point: LatLng(lat, lng),
-                width: 35,
-                height:35,
-                child: Icon(Icons.location_on, color: Colors.red,),
+          Expanded(
+            child: FlutterMap(
+              mapController: _mapController,
+              options: MapOptions(
+                initialCenter: LatLng(lat, lng), // Center the map over London
+                initialZoom: 5.6,
               ),
-            ],
-          ),
+              children: [
+                TileLayer( // Display map tiles from any source
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+                  userAgentPackageName: 'com.example.app',
+                  // And many more recommended properties!
+                ),
 
-          RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
-            attributions: [
-              TextSourceAttribution(
-                'OpenStreetMap contributors',
-                onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // (external)
-              ),
-              // Also add images...
-            ],
+                PolylineLayer(
+                  polylines: [
+                    Polyline(
+                      points: polylinePoints,
+                      strokeWidth: 4.0,
+                      color: Colors.blue,
+                    )
+                  ]
+                ),
+            
+                MarkerLayer(
+                  markers:  [
+                    Marker(
+                      point: LatLng(lat, lng),
+                      width: 70,
+                      height: 70,
+                      child: GestureDetector(
+                        child: Icon(Icons.location_on, color: Colors.red),
+                        onTap: () {
+                          print('Ubicacion actual');
+                        }),
+                    ),
+                  ],
+                ),
+            
+                RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
+                  attributions: [
+                    TextSourceAttribution(
+                      'OpenStreetMap contributors',
+                       onTap: () => print('OpenStreetMap contributors'),                        // (external)
+                    ),
+                    // Also add images...
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
---
+++ lib/services/location_service.dart
@@ -0,0 +1,54 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+// class LocationService{
+//   //static const url = 'http://127.0.0.1:8000/api/';
+//   static const url = 'http://192.168.56.1:8000/api/';
+//   Future getLocations() async {
+//     var response = await http.get(Uri.parse(url+'locations'));
+//     if(response.statusCode == 200){
+//       var jsonResponse = convert.jsonDecode(response.body);
+//       print(jsonResponse);
+//     }else{
+//       return 'error';
+//     }
+//   }
+// }
+
+class LocationService {
+  static const url = 'http://192.168.0.6:8000/api/';
+  
+  // Future<List<Map<String, dynamic>>> getLocations() async {
+  //   try {
+  //     var response = await http.get(Uri.parse(url + 'locations'));
+      
+  //     if (response.statusCode == 200) {
+  //       var jsonResponse = convert.jsonDecode(response.body);
+        
+  //       if (jsonResponse is List) {
+  //         return List<Map<String, dynamic>>.from(jsonResponse);
+  //       } else {
+  //         print('Error: Estructura JSON inesperada');
+  //         return [];  // Retorna una lista vacía en caso de error de estructura
+  //       }
+  //     } else {
+  //       print('Request failed with status: ${response.statusCode}');
+  //       return [];  // Retorna una lista vacía si la solicitud falla
+  //     }
+  //   } catch (e) {
+  //     print('Error al procesar la respuesta JSON: $e');
+  //     return [];  // Retorna una lista vacía en caso de excepción
+  //   }
+  // }
+
+    Future getLocations() async {
+    var response = await http.get(Uri.parse(url+'locations'));
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      return jsonResponse;
+    } else {
+      return 'Error de conexion';
+    }
+  }
+
+}
